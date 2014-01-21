0f255cb67e37255e9b0488179bed55643ca32939
victorc365 <vhcontreraso@unal.edu.co>
2014-01-21 00:14:50 -0500

    Add login method Add login method to persona facade, the method don´t yet complete.

--- src/java/com/superenvios/DAO/PersonaFacade.java
+++ src/java/com/superenvios/DAO/PersonaFacade.java
@@ -5,7 +5,11 @@
 package com.superenvios.DAO;
 
 import com.superenvios.model.Persona;
+import java.util.List;
+import javax.annotation.security.PermitAll;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -28,10 +32,21 @@ public class PersonaFacade extends AbstractFacade<Persona> implements PersonaFac
         super(Persona.class);
     }
     
+    @Override
+    @PermitAll
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public Persona login(String userName,String password){
+        List<Persona> personas;
         Persona p=null;
-        Query q1=em.createQuery("SELECT p FROM Persona p JOIN FETCH p.rol");
-        p=(Persona)q1.getSingleResult();
+        Query q1=em.createQuery("SELECT p FROM Persona p WHERE p.nombre = :userName AND p.password = :passwd");
+        q1.setParameter("userName", userName);
+        q1.setParameter("passwd", password);
+        personas=q1.getResultList();
+        if(!personas.isEmpty()){
+            System.out.println("Se recupero una entidad...");
+            System.out.println("nombre "+personas.get(0).getNombre());
+            return personas.get(0);
+        }
         return p;
     }
 }
